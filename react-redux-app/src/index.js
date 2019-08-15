c4ba089b03e8fd54f9cb9f11739a5170d5519714
ant <ren.meta@gmail.com>
2019-08-15 15:39:11 -0400

    set up store

--- react-redux-app/src/index.js
+++ react-redux-app/src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import {connect} from "redux";
+import {Provider} from "react-redux";
+
+import {weatherReducer} from "./reducers"
+
 import App from './App';
 import './index.css';
 
+const store= createStore(weatherReducer)
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+  <App />
+  </Provider>,
   document.getElementById('root')
 );
